82f5362376c387175b1862024ec86c6f763e99db
Jasper Fung <contact@jasperfung.co>
2017-11-21 15:01:24 -0800

    made pac man move over some nuggets

--- PacMan.py
+++ PacMan.py
@@ -34,7 +34,22 @@ class MovingBody(Agent):
         self.accel    = self.steer()
         self.world.trim(self)
 
-world = [ [0] * 40 + [1] * 20 ] * 45
+world = [ [1] * 45 ] * 60
+world[30] = [0] * 60
+world[31] = [0] * 60
+world[32] = [0] * 60
+for r in world:
+    r[30] = 0
+    r[31] = 0
+    r[32] = 0
+
+class Nugget(MovingBody):
+    def __init__(self, world, x, y):
+        position0 = Point2D(x, y)
+        MovingBody.__init__(self, position0, Vector2D(0), world)
+
+    def color(self):
+        return "#F0C080"
 
 class PacMan(MovingBody):
     
@@ -47,27 +62,69 @@ class PacMan(MovingBody):
         MovingBody.__init__(self,position0,velocity0,world)
         self.speed   = 0.0
         self.impulse = 0
+        self.angle = 90
+        self.direction = 'right'
 
     def color(self):
         return "#F0C080"
+    
+    def get_heading(self):
+        angle = self.angle * math.pi / 180.0
+        return Vector2D(math.cos(angle), math.sin(angle))
+
+    def shape(self):
+        h  = self.get_heading()
+        hp = h.perp()
+        print(hp)
+        pacShape = []
+        pacShape.append(self.position + Vector2D(-1, -2))
+        pacShape.append(self.position + Vector2D(1, -2))
+        pacShape.append(self.position + Vector2D(2, -1))
+        pacShape.append(self.position + Vector2D(2, 1))
+        pacShape.append(self.position + Vector2D(1, 2))
+        pacShape.append(self.position + Vector2D(-1, 2))
+        pacShape.append(self.position + Vector2D(-2, 1))
+        pacShape.append(self.position + Vector2D(-2, -1))
+        if self.direction == 'right':
+            pacShape.insert(3, self.position + Vector2D(0,0))
+        elif self.direction == 'up':
+            pacShape.insert(5, self.position + Vector2D(0,0))
+        elif self.direction == 'left':
+            pacShape.insert(7, self.position + Vector2D(0,0))
+        elif self.direction == 'down':
+            pacShape.insert(1, self.position + Vector2D(0,0))
+        return pacShape
         
     def turn_left(self):
+        self.direction = 'left'
         self.velocity = Vector2D(-0.5,0)
 
     def turn_right(self):
+        self.direction = 'right'
         self.velocity = Vector2D(0.5,0)
 
     def turn_up(self):
+        self.direction = 'up'
         self.velocity = Vector2D(0,0.5)
 
     def turn_down(self):
+        self.direction = 'down'
         self.velocity = Vector2D(0,-0.5)
 
     def update(self):
         MovingBody.update(self)
         x = int(numpy.interp(self.position.x, [-30, 30], [0, 60]))
         y = int(numpy.interp(self.position.y, [-22, 22], [0, 45]))
-        if world[0][x] == 1:
+        print(x,y)
+        if self.direction == 'right':
+            x += 1
+        elif self.direction == 'left':
+            x -= 1
+        elif self.direction == 'up':
+            y += 1
+        elif self.direction == 'down':
+            y -= 1
+        if world[x][y] == 1:
             self.velocity = Vector2D(0.0)
             
 class PlayPacMan(Game):
@@ -77,7 +134,7 @@ class PlayPacMan(Game):
     INTRODUCE_CHANCE = 0.01
     
     def __init__(self):
-        Game.__init__(self,"ASTEROIDS!!!",60.0,45.0,800,600,topology='wrapped')
+        Game.__init__(self,"PACMAN!!!",60.0,45.0,800,600,topology='wrapped')
 
         self.number_of_asteroids = 0
         self.number_of_shrapnel = 0
@@ -88,6 +145,13 @@ class PlayPacMan(Game):
         self.started = False
 
         self.PacMan = PacMan(self)
+        self.nuggets = []
+        for x in range(-30, 30):
+            for y in range(9, 10):
+                self.nuggets.append(Nugget(self, x, y))
+        for x in range(2, 3):
+            for y in range(-30, 30):
+                self.nuggets.append(Nugget(self, x, y))
 
     def handle_keypress(self,event):
         Game.handle_keypress(self,event)
@@ -116,7 +180,6 @@ class PlayPacMan(Game):
         Game.update(self)
         
 
-print("Hit j and l to turn, i to create thrust, and SPACE to shoot. Press q to quit.")
 game = PlayPacMan()
 while not game.GAME_OVER:
     time.sleep(1.0/60.0)
